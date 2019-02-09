8bb0894789beeeaacd9ea72ee64a6f6d7c78f5fd
KobayashiKz <anuuihsa9711212@gmail.com>
2019-02-09 11:10:45 +0900

    Mockを利用したテスト追加

--- app/src/androidTest/java/com/kk/qiitaclient/kotlinstartbookapp/qiitaclient/MainActivityTest.kt
+++ app/src/androidTest/java/com/kk/qiitaclient/kotlinstartbookapp/qiitaclient/MainActivityTest.kt
@@ -1,17 +1,22 @@
 package com.kk.qiitaclient.kotlinstartbookapp.qiitaclient
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
+import com.kk.qiitaclient.kotlinstartbookapp.qiitaclient.client.ArticleClient
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import rx.Observable
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -23,7 +28,7 @@ class MainActivityTest {
     val activityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun 各ビューが表示されていること_ただしプログレスバーは非表示() {
+    fun `各ビューが表示されていること_ただしプログレスバーは非表示`() {
         // onView()/withId/check などはEspressoが提供するAPI
         // onView: Espressoのstaticメソッド
         // onView(withId(R.id.list_view)) : テスト対象を操作や検証できるようにしている
@@ -36,5 +41,25 @@ class MainActivityTest {
         onView(withId(R.id.progress_bar)).check(matches(isNotDisplayed()))
     }
 
+    /**
+     * 非表示かどうか
+     */
     private fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
+
+    // Dagger2によるモックnoの差し込みを行うと綺麗
+    // Dagger2の機能を利用してモックオブジェクトをインジェクトする
+    @Test
+    fun `検索ボタンがタップされたら入力されたクエリ文字列で記事検索APIを叩くこと`() {
+        val articleClient = mock(ArticleClient::class.java).apply {
+            `when`(search("user:ngsw_taro")).thenReturn(Observable.just(listOf()))
+        }
+
+        activityTestRule.activity.articleClient = articleClient
+
+        onView(withId(R.id.query_edit_text)).perform(typeText("user:ngsw_taro"))
+        onView(withId(R.id.search_button)).perform(click())
+
+        verify(articleClient).search("user:ngsw_taro")
+    }
+
 }
